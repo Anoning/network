da3c4a864ece957856a13cedde7fd97459044bc9
Anoning <yitinh@foxmail.com>
2021-12-21 20:07:06 +0800

    del

--- 1easy_connect/build/clean.sh
+++
@@ -1,7 +0,0 @@
-rm -rf bin/
-rm -rf CMakeFiles/
-rm -rf lib/
-rm -rf 1easy_connect
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
--- mid-homework/build/clean.sh
+++
@@ -1,7 +0,0 @@
-rm -rf bin/
-rm -rf CMakeFiles/
-rm -rf lib/
-rm -rf mid_homework
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
